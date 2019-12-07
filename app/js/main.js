b9eac9ab5bd3386ab1b5652cf0bf83b4e3c3eb36
sfgardev <sf.garol@gmail.com>
2019-12-07 22:42:32 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,16 @@
 $(function(){
 
+    $('.featured__slider').slick({
+      prevArrow:'<button type="button" class="slick-prev icon-chevron-left"></button>',
+      nextArrow:'<button type="button" class="slick-next icon-chevron-right"></button>'
+    });
+
     $(".rateyo").rateYo({
         rating: 4.5,
         starWidth: "13px",
         normalFill: "#808080",
-        readOnly: true
+        readOnly: true,
+        spacing: '3px'
       });
 
 });
